e9802cca3b93fca586dd4eec3e107524cbba6697
asifahmed1810 <asifahmed18oct@gmail.com>
2025-12-06 11:37:10 +0600

    Added vehicles table and bookings table

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,12 +1,12 @@
-import {Pool} from "pg";
+import { Pool } from "pg";
 import config from ".";
 
-export const pool=new Pool({
-    connectionString:`${config.connection_str}`
-})
+export const pool = new Pool({
+  connectionString: `${config.connection_str}`,
+});
 
-const initDB=async()=>{
-    await pool.query(`
+const initDB = async () => {
+  await pool.query(`
         CREATE TABLE IF NOT EXISTS users(
         id SERIAL PRIMARY KEY,
         name VARCHAR(100) NOT NULL,
@@ -18,7 +18,36 @@ const initDB=async()=>{
         updated_at TIMESTAMP DEFAULT NOW()
           
         )
-        `)
-}
+        `);
+
+  await pool.query(`
+  CREATE TABLE IF NOT EXISTS vehicles(
+    id SERIAL PRIMARY KEY,
+    vehicle_name VARCHAR(100) NOT NULL,
+    type VARCHAR(20) NOT NULL CHECK (type IN ('car', 'bike', 'van', 'SUV')),
+    registration_number VARCHAR(50) UNIQUE NOT NULL,
+    daily_rent_price NUMERIC NOT NULL CHECK (daily_rent_price > 0),
+    availability_status VARCHAR(20) NOT NULL CHECK (availability_status IN ('available', 'booked')),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+  )
+`);
+  await pool.query(`
+  CREATE TABLE IF NOT EXISTS bookings(
+    id SERIAL PRIMARY KEY,
+    customer_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    vehicle_id INT NOT NULL REFERENCES vehicles(id) ON DELETE CASCADE,
+    
+    rent_start_date DATE NOT NULL,
+    rent_end_date DATE NOT NULL CHECK (rent_end_date > rent_start_date),
+    
+    total_price NUMERIC NOT NULL CHECK (total_price > 0),
+    status VARCHAR(20) NOT NULL CHECK (status IN ('active', 'cancelled', 'returned')),
+    
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+  )
+`);
+};
 
 export default initDB;
